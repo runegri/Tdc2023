2dc2b38de95805e4dc178f872a926a75991b7034
Rune Grimstad <rune.andreas.grimstad@nhn.no>
2023-10-10 21:59:49 +0200

    Skriver innhold til konsollvindu

--- OAuth2Demo/Program.cs
+++ OAuth2Demo/Program.cs
@@ -1,28 +1,87 @@
 ﻿
-// Create a client handler that uses the proxy
-var httpClientHandler = new HttpClientHandler
+var tokenResponse = await GetTokenResponse(new()
 {
-    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-};
+    {"client_id", "175089f1-f230-4e66-8151-100ffb2f24a5" },
+    {"scope", "udelt:test-api/api" },
+    {"grant_type", "client_credentials" },
+    {"client_secret", "CGti1jJLqU-HRH7sbGjKWbQFqNhnCkJPaebR6bLgdvwgt5Y_L0wnUqIlQ8otJapa" }
+});
 
-var client = new HttpClient(httpClientHandler);
 
-var tokenContent = new FormUrlEncodedContent(new[]
+
+Console.ReadLine();
+Console.WriteLine("Got the following response:");
+PrintJson(tokenResponse);
+
+Console.ReadLine();
+Console.WriteLine("Decoding the Access Token:");
+PrintJwt(GetAccessToken(tokenResponse));
+
+
+
+
+
+
+async Task<string> GetTokenResponse(Dictionary<string, string> parameters)
 {
-    new KeyValuePair<string, string>("client_id", "175089f1-f230-4e66-8151-100ffb2f24a5"),
-    new KeyValuePair<string, string>("scope", "udelt:test-api/api"),
-    new KeyValuePair<string, string>("grant_type", "client_credentials"),
-    new KeyValuePair<string, string>("client_secret", "CGti1jJLqU-HRH7sbGjKWbQFqNhnCkJPaebR6bLgdvwgt5Y_L0wnUqIlQ8otJapa"),
-  });
+    var content = new FormUrlEncodedContent(parameters);
 
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Calling the token endpoint with parameters:");
 
-var response = await client.PostAsync("https://helseid-sts.utvikling.nhn.no/connect/token", tokenContent);
+    foreach(var parameter in parameters)
+    {
+        Console.WriteLine($"{parameter.Key} => {parameter.Value}");
+    }
 
+    var client = new HttpClient();
 
-if(response.IsSuccessStatusCode)
-{
-    var result = await response.Content.ReadAsStringAsync();
-    Console.WriteLine(result);
+    var response = await client.PostAsync("https://helseid-sts.utvikling.nhn.no/connect/token", content);
+    if (response.IsSuccessStatusCode)
+    {
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    return $"Returned status code {response.StatusCode}";
 }
 
+void PrintJson(string json)
+{
+    try
+    {
+        using var jDoc = System.Text.Json.JsonDocument.Parse(json);
+        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(jDoc, new System.Text.Json.JsonSerializerOptions { WriteIndented = true }));
+    }
+    catch
+    {
+        Console.WriteLine(json);
+    }
+}
 
+string GetAccessToken(string tokenResponse)
+{
+    using var jDoc = System.Text.Json.JsonDocument.Parse(tokenResponse);
+    var at = jDoc.RootElement.GetProperty("access_token").GetString() ?? "";
+    return at;
+}
+
+void PrintJwt(string jwt)
+{
+    var jwtParts = jwt.Split('.');
+    var header = Microsoft.IdentityModel.Tokens.Base64UrlEncoder.Decode(jwtParts[0]);
+    var body = Microsoft.IdentityModel.Tokens.Base64UrlEncoder.Decode(jwtParts[1]);
+
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Header:");
+    PrintJson(header);
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Body:");
+    PrintJson(body);
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Signature:");
+    Console.WriteLine(jwtParts[2]);
+
+}
--- OidcDemo/Program.cs
+++ OidcDemo/Program.cs
@@ -2,7 +2,6 @@
 
 using System.Diagnostics;
 using System.Net;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Web;
 
@@ -10,43 +9,43 @@ var clientId = "175089f1-f230-4e66-8151-100ffb2f24a5";
 var scope = HttpUtility.UrlEncode("openid profile helseid://scopes/identity/pid udelt:test-api/api");
 var redirectUri = HttpUtility.UrlEncode("http://localhost:12345/callback");
 
-var authorizeUrl = $"http://helseid-sts.utvikling.nhn.no/connect/authorize?client_id={clientId}&scope={scope}&redirect_uri={redirectUri}&response_type=code";
+var authorizationServer = "https://helseid-sts.utvikling.nhn.no";
+
+var authorizeUrl = $"{authorizationServer}/connect/authorize?client_id={clientId}&scope={scope}&redirect_uri={redirectUri}&response_type=code";
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine($"Running system browser against url {authorizeUrl}");
 
 var authResponse = await RunBrowser(authorizeUrl, "http://localhost:12345/");
-
-Console.WriteLine(authResponse["code"]);
-
 var code = authResponse["code"];
 
-// Create a client handler that uses the proxy
-var httpClientHandler = new HttpClientHandler
-{
-    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-};
+Console.WriteLine($"Authorization code received: {code}");
+Console.WriteLine();
 
-var client = new HttpClient(httpClientHandler);
+Console.ReadLine();
 
-var tokenContent = new FormUrlEncodedContent(new[]
-{
-    new KeyValuePair<string, string>("client_id", "175089f1-f230-4e66-8151-100ffb2f24a5"),
-    new KeyValuePair<string, string>("code", code),
-    new KeyValuePair<string, string>("redirect_uri", "http://localhost:12345/callback"),
-    new KeyValuePair<string, string>("grant_type", "authorization_code"),
-    new KeyValuePair<string, string>("client_secret", "CGti1jJLqU-HRH7sbGjKWbQFqNhnCkJPaebR6bLgdvwgt5Y_L0wnUqIlQ8otJapa"),
-  });
+var tokenResponse = await GetTokenResponse(new() {
+    { "client_id", clientId },
+    { "grant_type", "authorization_code" },
+    { "client_secret", "CGti1jJLqU-HRH7sbGjKWbQFqNhnCkJPaebR6bLgdvwgt5Y_L0wnUqIlQ8otJapa" },
+    { "redirect_uri", "http://localhost:12345/callback"},
+    { "code", code},
+});
 
+Console.WriteLine("Got the following response:");
+PrintJson(tokenResponse);
 
-var response = await client.PostAsync("https://helseid-sts.utvikling.nhn.no/connect/token", tokenContent);
+Console.ReadLine();
+Console.WriteLine("Decoding the Identity Token:");
+PrintJwt(GetToken(tokenResponse, "id_token"));
 
-var result = await response.Content.ReadAsStringAsync();
-
-if (response.IsSuccessStatusCode)
-{
-    Console.WriteLine(result);
-}
+Console.ReadLine();
+Console.WriteLine("Decoding the Access Token:");
+PrintJwt(GetToken(tokenResponse, "access_token"));
 
 
-async Task<Dictionary<string,string>> RunBrowser(string url, string redirect)
+
+async Task<Dictionary<string, string>> RunBrowser(string url, string redirect)
 {
     url = url.Replace("&", "^&");
     Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
@@ -67,6 +66,70 @@ async Task<Dictionary<string,string>> RunBrowser(string url, string redirect)
     var request = q.AllKeys.ToDictionary(k => k, k => q[k]);
 
     listener.Stop();
-    
+
     return request;
+}
+
+async Task<string> GetTokenResponse(Dictionary<string, string> parameters)
+{
+    var content = new FormUrlEncodedContent(parameters);
+
+    Console.ResetColor();
+    Console.WriteLine("Calling the token endpoint with parameters:");
+
+    foreach (var parameter in parameters)
+    {
+        Console.WriteLine($"{parameter.Key} => {parameter.Value}");
+    }
+
+    var client = new HttpClient();
+
+    var response = await client.PostAsync("https://helseid-sts.utvikling.nhn.no/connect/token", content);
+    if (response.IsSuccessStatusCode)
+    {
+        return await response.Content.ReadAsStringAsync();
+    }
+
+    return $"Returned status code {response.StatusCode}";
+}
+
+void PrintJson(string json)
+{
+    try
+    {
+        using var jDoc = System.Text.Json.JsonDocument.Parse(json);
+        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(jDoc, new System.Text.Json.JsonSerializerOptions { WriteIndented = true }));
+    }
+    catch
+    {
+        Console.WriteLine(json);
+    }
+}
+
+string GetToken(string tokenResponse, string tokenType)
+{
+    using var jDoc = System.Text.Json.JsonDocument.Parse(tokenResponse);
+    var at = jDoc.RootElement.GetProperty(tokenType).GetString() ?? "";
+    return at;
+}
+
+void PrintJwt(string jwt)
+{
+    var jwtParts = jwt.Split('.');
+    var header = Microsoft.IdentityModel.Tokens.Base64UrlEncoder.Decode(jwtParts[0]);
+    var body = Microsoft.IdentityModel.Tokens.Base64UrlEncoder.Decode(jwtParts[1]);
+
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Header:");
+    PrintJson(header);
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Body:");
+    PrintJson(body);
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Signature:");
+    Console.WriteLine(jwtParts[2]);
+
 }
